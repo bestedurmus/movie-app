53cc9aaf40fa09596ac604782a8a52e3e63eba2d
beste <bestebozpinar@gmail.com>
2022-04-06 11:52:26 +0300

    v3

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,22 @@
+import { Container, Grid, Input } from '@mui/material'
 import React from 'react'
+import MovieCard from '../components/MovieCard'
+
+const FEATURED_API = 'https://api.themoviedb.org/3/discover/movie?api_key=d6278b3dc3e6f8f8376a89851c3f8c8f'
+const SEARCH_API = 'https://api.themoviedb.org/3/search/movie?api_key=d6278b3dc3e6f8f8376a89851c3f8c8f&query='
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <div>
+      <Grid container>
+        <Input placeholder="Search a movie " type='search' sx={{margin:"1rem auto", width:"50%"}}/>
+      </Grid>
+
+      <Container>
+        <MovieCard/>
+      </Container>
+      
+    </div>
   )
 }
 
