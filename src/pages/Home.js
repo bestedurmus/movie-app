03988188f56ef75591eaa915376372e22619dd31
beste <bestebozpinar@gmail.com>
2022-04-06 12:57:29 +0300

    v4

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,19 +1,53 @@
 import { Container, Grid, Input } from '@mui/material'
-import React from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import MovieCard from '../components/MovieCard'
+import { AuthContext } from '../context/AuthContext'
 
 const FEATURED_API = 'https://api.themoviedb.org/3/discover/movie?api_key=d6278b3dc3e6f8f8376a89851c3f8c8f'
 const SEARCH_API = 'https://api.themoviedb.org/3/search/movie?api_key=d6278b3dc3e6f8f8376a89851c3f8c8f&query='
 
 const Home = () => {
+  const [movies, setMovies] = useState([])
+  const [searchMovie, setSearchMovie] = useState('')
+  const { currentUser } = useContext(AuthContext)
+
+  useEffect(() => {
+    getMovies(FEATURED_API)
+  
+    
+  }, [])
+
+  const getMovies = (API) => {
+    fetch(API)
+    .then(res => res.json())
+    .then(res => setMovies(res.results))
+    
+  }
+
+  const handleSubmit = () => {
+    if(currentUser && searchMovie){
+      getMovies(SEARCH_API + searchMovie)
+    }else{
+      alert("Please log in to search a movie")
+    }
+  }
+
+
+
+
   return (
     <div>
       <Grid container>
-        <Input placeholder="Search a movie " type='search' sx={{margin:"1rem auto", width:"50%"}}/>
+        <form style={{width:"100%", display:"flex"}} onSubmit={() => handleSubmit()}>
+          <Input onChange={(e) => setSearchMovie(e.target.value)} value={searchMovie} placeholder="Search a movie " type='search' sx={{margin:"1rem auto", width:"50%"}}/>
+        </form>
       </Grid>
 
-      <Container>
-        <MovieCard/>
+      <Container sx={{display:"flex", flexWrap:"wrap"}}>
+        {movies.map((movie)=> (
+          <MovieCard key={movie.id} {...movie}/>
+        ))}
+        
       </Container>
       
     </div>
